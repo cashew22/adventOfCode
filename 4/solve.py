1f831a454ae52667b2fa5139630eb09a5350c620
Eric Tremblay <etremblay.15@ens.etsmtl.ca>
2015-12-04 09:04:54 -0500

    Day 4 solve

---
+++ 4/solve.py
@@ -0,0 +1,19 @@
+import hashlib
+seq = 1
+m = hashlib.md5()
+
+while not m.hexdigest().startswith('00000'):
+	m = hashlib.md5()
+	seq+=1
+	m.update('iwrupvqb%d'%seq)
+
+print  "this is with five 0: ",seq
+
+seq = 0
+
+while not m.hexdigest().startswith('000000'):
+	m = hashlib.md5()
+	seq+=1
+	m.update('iwrupvqb%d'%seq)
+
+print  "this is with six 0: ",seq
