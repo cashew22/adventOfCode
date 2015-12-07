db942b0b83c71c0123a15fa6d42900a92416c1e2
Eric Tremblay <etremblay.15@ens.etsmtl.ca>
2015-12-07 11:09:33 -0500

    day6/2

--- 6/solve.py
+++ 6/solve.py
@@ -2,6 +2,7 @@ import numpy as np
 
 f = open('input.txt', 'r')
 grid = np.zeros( (1000,1000), dtype=bool )
+gridlumen = np.zeros( (1000,1000), dtype=int)
 index = 0
 
 for l in f:
@@ -21,11 +22,14 @@ for l in f:
 
 	if ops == "toggle":
 		grid[start[0]:stop[0] + 1, start[1]:stop[1] + 1] = np.invert(grid[start[0]:stop[0] + 1, start[1]:stop[1] + 1], dtype=bool)
+		gridlumen[start[0]:stop[0] + 1, start[1]:stop[1] + 1] += 2
 	elif ops == "on":
-		mask = grid[start[0]:stop[0] + 1, start[1]:stop[1] + 1]
-		mask.fill(True)
+		grid[start[0]:stop[0] + 1, start[1]:stop[1] + 1].fill(True)
+		gridlumen[start[0]:stop[0] + 1, start[1]:stop[1] + 1] += 1
 	else:
-		mask = grid[start[0]:stop[0] + 1, start[1]:stop[1] + 1]
-		mask.fill(False)
+		grid[start[0]:stop[0] + 1, start[1]:stop[1] + 1].fill(False)
+		gridlumen[start[0]:stop[0] + 1, start[1]:stop[1] + 1] -= 1
+		gridlumen[gridlumen < 0] = 0
 
-print np.sum(grid)
+print "The number of light turn on is :", np.sum(grid)
+print "The number of lumen use is :", np.sum(gridlumen)
